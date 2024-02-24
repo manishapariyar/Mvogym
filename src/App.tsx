d15bcce90d4945ae086a2081e14a2312ead9c700
manishapariyar <manishapariyar196@gmail.com>
2024-02-24 07:09:52 +0545

    navbar is compelete

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 
+import Navbar from "@/components/navbar/Navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/components/shared/types";
+
+
 
 const App = () => {
+
+  const [selectedPage,setSelectedPage] = useState<
+  SelectedPage>(SelectedPage.Home);
+  const [isTopOfpage,setIsTopOfpage] = useState<boolean>(true);
+
+   useEffect(()=>{
+    const handleScroll =() =>{
+      if(window.scrollY === 0){
+        setIsTopOfpage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      // if(window.scrollY !== 0)
+      else
+      {
+        setIsTopOfpage(false);
+      }
+    }
+    window.addEventListener("scroll",handleScroll);
+    return ()=> window.removeEventListener("scroll", handleScroll)
+   },[]);
   return (
-    <div>App</div>
+   <div className="app bg-gray-20">
+    <Navbar 
+   isTopOfpage ={isTopOfpage}
+    selectedPage={selectedPage}  setSelectedPage={setSelectedPage}/>
+   </div>
   )
 }
 
